654af830ae60b43bdcb9c540a2186b3e45f26c09
Paulo <paulohenriquecarvalho607@gmail.com>
2026-07-08 22:52:53 -0300

    Fix: script admin

--- backend/DjangoProject/create_admin.py
+++ backend/DjangoProject/create_admin.py
@@ -1,12 +1,16 @@
 import os
 import django
-from django.contrib.auth.models import User
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 django.setup()
 
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@admin.com', 'admin')
-    print('Superusuário criado com sucesso!')
-else:
-    print('O usuário já existe.')
+from django.contrib.auth.models import User
+
+# Tenta criar e se der erro, apenas ignora
+try:
+    if not User.objects.filter(username='admin').exists():
+        user = User.objects.create_superuser('admin', 'admin@admin.com', 'Admin123456!')
+        print('Superusuário criado com sucesso!')
+    else:
+        print('O usuário já existe.')
+except Exception as e:
+    print(f'Erro ao criar superusuário: {e}')
